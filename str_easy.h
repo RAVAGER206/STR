200ae87333e87a399c0f10f27041fdaec471312f
RAVAGER206 <114873479+RAVAGER206@users.noreply.github.com>
2022-12-12 14:10:30 +0300

    Add files via upload

---
+++ str_easy.h
@@ -0,0 +1,18 @@
+#ifndef STR_EASY_H_INCLUDED
+#define STR_EASY_H_INCLUDED
+#include <string>
+#include "str_easy.h"
+using namespace std;
+
+string itc_hello_str(string name); // 1
+
+long long itc_len(string str); // 2
+
+void itc_print_copy_str(string str, int a); // 3
+
+void itc_first_end_three(string str); // 4
+
+int itc_count_char_in_str(char ch, string str); // 5
+
+#endif // STRING_H_INCLUDED
+
