2f4c71415bc988a434b36081e0ca4e9e5e61ade6
RAVAGER206 <114873479+RAVAGER206@users.noreply.github.com>
2022-12-25 18:59:49 +0300

    Update str_easy.h

--- str_easy.h
+++ str_easy.h
@@ -8,10 +8,6 @@ string itc_hello_str(string n);
 
 long long itc_len(string n); 
 
-void itc_print_copy_str(string n, int a); 
-
-void itc_first_end_three(string n); 
-
 int itc_count_char_in_str(char b, string n);
 
 string itc_even_place(string n); 
@@ -24,9 +20,12 @@ string itc_slice_str(string n, int z, int v);
 
 bool itc_equal_reverse(string n); 
 
+void itc_first_end_three(string n); 
+
 string itc_cmp_str(string n, string m, int u); 
 
 int itc_find_str(string s1, string s2);
 
 string itc_three_str(string s1, string s2, string s3);
   
+void itc_print_copy_str(string n, int a);
