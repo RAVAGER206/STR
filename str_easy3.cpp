61af98c66df840a05d55dd77ade8b52492adf9a2
RAVAGER206 <114873479+RAVAGER206@users.noreply.github.com>
2022-12-26 14:14:41 +0300

    Update str_easy3.cpp

--- str_easy3.cpp
+++ str_easy3.cpp
@@ -41,3 +41,44 @@ return i - (k - 1);
 return -1;
 } // end
 
+
+string itc_three_str (string s1, string s2, string s3){ // 13
+string sp, sv;
+int l, i;
+l = 0;
+sp = "\0";
+for (i = 0; s1[i] != '\0'; i++){
+if (s1[i] == s2[l]){
+sp = sp + s1[i];
+l++;
+if (sp == s2){
+sv = sv + s3;
+sp = "\0";
+}
+}
+else{
+sv = sv + s1[i];
+l = 0;
+sp = "\0";
+}
+}
+return sv;
+} // end
+
+
+int itc_max_char_on_end(string n){ // 14
+char z;
+int i, g, g1;
+z = '/';
+for (i = 0; n[i] != '\0'; i++){
+z = n[i];
+if (z > 47 && z < 58 && (n[i+1] == n[i] || n[i] == n[i-1])){
+g++;
+if (g >= g1)
+g1 = g;
+}
+else
+g = 0;
+}
+return g1 - 3;
+} // end
