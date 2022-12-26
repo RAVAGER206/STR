20072f0837948a7d877d3e885310eda7968a7714
RAVAGER206 <114873479+RAVAGER206@users.noreply.github.com>
2022-12-26 13:46:17 +0300

    Update str_easy.h

--- str_easy.h
+++ str_easy.h
@@ -5,26 +5,29 @@
 using namespace std;
 
 
+string itc_hello_str(string n);
+
 long long itc_len(string n); 
 
-int itc_count_char_in_str(char b, string n);
+void itc_print_copy_str(string n, int a);
+
+void itc_first_end_three(string n); 
+
+int itc_count_char_in_str(char b, string n); 
 
 string itc_even_place(string n); 
 
-string itc_hello_str(string n); 
-
 double itc_percent_lower_uppercase(string n); 
 
-string itc_reverse_str(string n);
+string itc_reverse_str(string n); 
 
 string itc_slice_str(string n, int z, int v); 
 
 bool itc_equal_reverse(string n); 
 
-void itc_first_end_three(string n); 
-
 string itc_cmp_str(string n, string m, int u); 
 
-int itc_find_str(string s1, string s2);
-  
-void itc_print_copy_str(string n, int a);
+int itc_find_str (string s1, string s2); 
+
+
+#endif // STREASY_H_INCLUDED
