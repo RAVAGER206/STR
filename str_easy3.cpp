23ef43acefe2394ab06a5c54557be69a8532a3b3
RAVAGER206 <114873479+RAVAGER206@users.noreply.github.com>
2022-12-26 18:25:29 +0300

    Update str_easy3.cpp

--- str_easy3.cpp
+++ str_easy3.cpp
@@ -7,8 +7,9 @@ using namespace std;
 string itc_cmp_str(string n, string m, int u){ // 11
 string o;
 int i, l, d;
+o = "";
 d = itc_len(n);
-if (m[0] == '\0')
+if (m == "")
 return n;
 for (i = 0; i < u; i++)
 o = o + n[i];
@@ -24,19 +25,22 @@ return o;
 } // end
 
 
-int itc_find_str(string s1, string s2){ // 12
-int i, l, k;
+int itc_find_str (string s1, string s2){ // 12
+int i, l;
 string o;
 i = 0;
 l = 0;
-k = 0;
 for (i = 0; s1[i] != '\0'; i++){
 if (s1[i] == s2[l]){
 o = o + s1[i];
 l++;
 }
+else{
+o = "\0";
+l = 0;
+}
 if (o == s2)
-return i - (k - 1);
+return i - (l - 1);
 }
 return -1;
 } // end
